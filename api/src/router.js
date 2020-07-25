32ef3568c925ab564f4e4dcb202cd8a72472b8bb
Daniel <KasumiL5x@gmail.com>
2020-07-25 20:52:18 +0100

    'fixes' but it was just reordering code >_>

--- api/src/router.js
+++ api/src/router.js
@@ -32,13 +32,12 @@ const upload = multer({
 router.post("/upload", upload.single("photo"), async (request, response) => {
 	if(request.fileValidationError) {
 		return response.status(400).json({error: request.fileValidationError});
-	} else {
-		try {
-			await imageProcessor(request.file.filename);
-		} catch (error) {
-		}
-		return response.status(201).json({success: true});
 	}
+	try {
+		await imageProcessor(request.file.filename);
+	} catch (error) {
+	}
+	return response.status(201).json({success: true});
 });
 
 router.get("/photo-viewer", (request, response) => {
